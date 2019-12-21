f0473eaeb1147a18a6d17d6e9bb531cc32fbda87
Ben Marden <benvmarden@gmail.com>
2019-12-20 17:33:26 -0800

    brew.sh

---
+++ brew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+
+# Install MacOS Applications
+brew cask install discord
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install dropbox
+

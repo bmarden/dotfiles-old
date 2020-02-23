94b03400b4d3fd26df6472198b404068a10f73a1
Ben <benvmarden@gmail.com>
2020-02-23 01:22:07 -0800

    Fix some errors in scripts

--- linux/scripts/programs/vsc.sh
+++ linux/scripts/programs/vsc.sh
@@ -2,7 +2,12 @@
 
 sudo snap install --classic code
 
-ln -s $(pwd)/../../vscode/vsc_settings.json $HOME/.config/Code/User/settings.json
+dest=$HOME/.config/Code/User/settings.json
+dateStr=$(date +%Y-%m-%d-%H%M) 
+if [ -f "${dest}" ]; then
+	mv ${dest}{,.${dateStr}}
+fi
+ln -s $(pwd)/../../vsc_settings.json ${dest}
 
 function install {
   name="${1}"
--- linux/scripts/programs/z_shell.sh
+++ linux/scripts/programs/z_shell.sh
@@ -4,16 +4,18 @@
 
 sudo apt install -y zsh
 
+cd $HOME
+
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/bmarden/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/bmarden/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Powerlevel 10k installation
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/bmarden/.oh-my-zsh/custom/themes/powerlevel10k
 
 

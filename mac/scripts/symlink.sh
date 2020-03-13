f0aa5640a871fc52ce1c88a89bcd47dece486d76
Ben Marden <benvmarden@gmail.com>
2020-03-13 00:19:37 -0700

    Fix vscode_settings symlink mac

--- mac/scripts/symlink.sh
+++ mac/scripts/symlink.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-dotfilesDir=$(pwd)
+dotfilesDir=$(pwd) 
 
 function linkDotfile {
 	dotfile=${1}
@@ -12,26 +12,26 @@ function linkDotfile {
 	if [ -h ~/${1} ]; then
 		# Existing symlink
 		echo "Removing existing symlink: ${dest}"
-		rm ${dest}
+		rm "${dest}"
 
 	elif [ -f "${dest}" ]; then
 		# Existing file
 		echo "Backing up existing file: ${dest}"
-		mv ${dest}{,.${dateStr}}
+		mv "${dest}"{,.${dateStr}}
 	
 	elif [ -d "${dest}" ]; then
 		# Existing dir
 		echo "Backing up existing dir: ${dest}"
-		mv ${dest}{,.${dateStr}}
+		mv "${dest}"{,.${dateStr}}
 	fi
 
 	echo "Creating new symlink: ${dest}"
-	ln -s ${dotfilesDir}/${1} ${dest}
+	ln -s "${dotfilesDir}/${1}" "${dest}"
 }
 
 linkDotfile .zshrc ${HOME}/.zshrc 
 linkDotfile .gitconfig ${HOME}/.gitconfig
 linkDotfile init.vim ${HOME}/.config/nvim/init.vim
 linkDotfile .p10k.zsh ${HOME}/.p10k.zsh
-
+linkDotfile vscode_settings.json "${HOME}/Library/Application Support/Code/User/settings.json" 
 

ae841b61d6e4ef5ff5b627ffe7fcf9562c886c94
Ben Marden <benvmarden@gmail.com>
2020-04-13 12:42:22 -0700

    Update vscode ext, vsc settings, init.vim

--- linux/scripts/programs/vsc.sh
+++ linux/scripts/programs/vsc.sh
@@ -7,18 +7,38 @@ function install {
   code --install-extension ${name} --force
 }
 
-install batisteo.vscode-django
-install bibhasdn.django-html 
-install dart-dart-code.dart-code
-install dart-code.flutter
-install liviuschera.noctis
-install dracula-theme.theme-dracula
-install equinusocio.vsc-material-theme-icons
+# Language packs
 install faustinoaq.lex-flex-yacc-bison
-install jolaleye.horizon-theme-vscode
 install luniclynx.lex
 install ms-python.python
 install ms-vscode.cpptools
+
+# Django stuff
+install batisteo.vscode-django
+install bibhasdn.django-html 
+
+# Flutter/Dart
+install dart-dart-code.dart-code
+install dart-code.flutter
+install jeroen-meijer.pubspec-assist
+install nash.awesome-flutter-snippets
+
+# Markdown
+install yzhang.markdown-all-in-one
+install davidanson.vscode-markdownlint
+
+# Utility
+install coenraads.bracket-pair-colorizer
+install usernamehw.errorlens
+install mhutchie.git-graph
+install asvetliakov.vscode-neovim
+
+# Themes/Styling
+install liviuschera.noctis
+install dracula-theme.theme-dracula
+install equinusocio.vsc-material-theme-icons
+install jolaleye.horizon-theme-vscode
 install pkief.material-icon-theme
 install asvetliakov.vscode-neovim
 install monokai.theme-monokai-pro-vscode
+install jeff-hykin.better-cpp-syntax

e4d2c4659e1e218ab111010db4a0110f54e64c6e
Ben Marden <benvmarden@gmail.com>
2021-03-13 15:07:46 -0800

    Fix path error in setupZsh

--- functions.sh
+++ functions.sh
@@ -40,7 +40,7 @@ function setupZsh {
   git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
   # Install zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
   # Powerlevel 10k installation
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k

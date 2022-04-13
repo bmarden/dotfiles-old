a4a22d4cc5e444cde3e79741f4e446d9201d5eb6
Ben Marden <benvmarden@gmail.com>
2022-04-13 00:05:38 -0700

    comment out unused installs

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -o nounset  # Error when unset variables
 set -o errexit  # Exit when error encountered
 
@@ -10,34 +11,27 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   dotfilePath="$HOME/dotfiles/linux"
   sudo apt update
 
-  printf("Installing apt pakages...\n")
+  printf "Installing apt pakages...\n"
   # apt programs to install
   installAptPackage curl
-  installAptPackage git
-  installAptPackage vim 
-  installAptPackage xclip
-  installAptPackage valgrind
+  # installAptPackage git
+  # installAptPackage vim 
+  # installAptPackage xclip
+  # installAptPackage valgrind
   # installAptPackage texlive-latex-extra
   installAptPackage htop
   # Python
   # sudo apt install -y python3-venv python3-pip
 
-  # Install Dropbox
-  cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-
-  # Install nvm
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-
   # Useless things
-  # installAptPackage figlet
-  # installAptPackage lolcat
+  installAptPackage figlet
+  installAptPackage lolcat
 
-  CUSTOM_NVIM_PATH=/usr/local/bin/nvim
+  CUSTOM_NVIM_PATH=/usr/bin/nvim
   # Install neovim  
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-  sudo mv nvim.appimage ${CUSTOM_NVIM_PATH}
-  chmod +x ${CUSTOM_NVIM_PATH}
+  # curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  # sudo mv nvim.appimage ${CUSTOM_NVIM_PATH}
+  # chmod +x ${CUSTOM_NVIM_PATH}
   
   
   set -u
